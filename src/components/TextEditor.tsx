f13aa4e45f6f07e7a90e18f91f610dfe03493e07
Yasmin <yasminmotahhary@gmail.com>
2025-04-14 01:49:53 -0600

    update readme

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function TextEditor() {
   const initialContent =
-    "welcome to my desktop! \n\nhere, you can: \n✨ click on the desktop icons to see my projects, skills, and more \n✨ open, close, and move windows \n✨ and much more to come...\n\nhave fun exploring!  \n\n- yasmin";
+    "welcome to my desktop! \n\nhere, you can: \n✨ click on the desktop icons to learn more about me\n✨ open, close, and move windows \n✨ and more to come soon...\n\nhave fun exploring!  \n\n- yasmin";
 
   const [text, setText] = useState(initialContent);
 
