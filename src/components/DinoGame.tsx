f1ae4a116ef6c6b4673fe4ec32fe5bb7da149db1
Yasmin <yasminmotahhary@gmail.com>
2025-09-02 14:41:27 -0400

    fixes

--- src/components/DinoGame.tsx
+++ src/components/DinoGame.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
 import ChromeDino from "react-chrome-dino";
 import "../styles/DinoGame.css";
 
 export default function DinoGame() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    // Ensure component is properly mounted before rendering ChromeDino
+    setIsMounted(true);
+
+    return () => {
+      // Cleanup when component unmounts
+      setIsMounted(false);
+    };
+  }, []);
+
+  if (!isMounted) {
+    return (
+      <div className="dino-game-container">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            color: "#666",
+          }}
+        >
+          Loading game...
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="dino-game-container">
-      <ChromeDino />
+      <ChromeDino key="chrome-dino-game" />
     </div>
   );
 }
